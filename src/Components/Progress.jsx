339d2cbf24c95cec8130da45b260537994279153
JamalDunn23 <jamaldunn23@gmail.com>
2023-07-29 15:56:38 -0400

    Start Of Lab

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,16 @@
-export default function Progress() {
-  return null;
+import React from "react";
+
+function Progress() {
+  return (
+    <div>
+      <section className="progress">
+        <h2>
+          Raised <span className="secondary">$0</span> of
+          <span className="secondary">$1000</span>
+        </h2>
+      </section>
+    </div>
+  );
 }
+
+export default Progress;
--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,17 @@
-export default function RecentDonations() {
-  return null;
+import React from 'react'
+
+function RecentDonations() {
+  return (
+    <div>
+      <section>
+  <h2>Recent Donations</h2>
+  <ul>
+    <li><span>Jo donated $25</span>You really need this. Really.</li>
+    <li><span>Rami donated $10</span>Here, take a break from work!</li>
+  </ul>
+</section>
+    </div>
+  )
 }
+
+export default RecentDonations
